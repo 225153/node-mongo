c50d715eb00692500e46aac7ed1497beaee204b9
omar kharrat <omar.kharrat.ps@gmail.com>
2025-09-29 15:35:57 +0200

    user validation

--- config/routes/user.js
+++ config/routes/user.js
@@ -16,16 +16,27 @@ router.post('/signup', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-    let data = req.body;
-    User.findOne({ username: data.username, password: data.password })
-        .then(user => {
-            if (user) {
-                res.status(200).send('Login successful');
-            } else {
-                res.status(401).send('Invalid username or password');
+    let { email, password } = req.body;
+    User.findOne({ email: email })
+        .then(
+            (user) => {
+                if (user) {
+                    if (bcrypt.compareSync(password, user.password)) {
+                        user.password = '****';
+                        res.status(200).send(user);
+                    } else {
+                        res.status(401).send('Invalid password');
+                    }
+                } else {
+                    res.status(404).send('User not found');
+                }
             }
-        })
-        .catch(err => res.status(400).send('Error logging in: ' + err.message));
+        )
+
+        .catch(err => res.status(500).send('Error logging in: ' + err.message));
+    
+
+        
 });
     
 
